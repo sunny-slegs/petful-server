d23a38c480b0c96a0d00b43430ec28f235a14eeb
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-08-03 13:54:11 -0400

    delete dog array items

--- index.js
+++ index.js
@@ -23,37 +23,57 @@ app.use(
   })
 );
 
+const cat = [
+  {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
 
+}]
+
+const dog = [ {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+},
+{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: ' music.',
+  name: 's',
+  sex: 'feMale',
+  age: 5,
+  breed: 'blue Retriever',
+  story: 'Owned'
+}
+
+
+]
 
 app.get('/api/cat', (req, res) => {
-  const data = {
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  };
-  res.json(data);
-  
+  res.json(cat[0]);
 });
 
 app.get('/api/dog', (req, res) => {
-  const data = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
-  };
-  res.json(data);
+  res.json(dog[0]);
 });
 
 app.get('/', (req, res) => {
   res.json({greeting: 'hello'});
+});
+
+
+
+app.delete(`/api/dog`, (req,res) => {
+  res.json(dog.shift());
+  res.sendStatus(204);
 });
 
 function runServer(port = PORT) {
