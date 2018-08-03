59830a212e59d6ebaaaa52ebd3b6a307b61c1b29
Sonya Slegers <sonjay08@gmail.com>
2018-08-03 11:22:52 -0500

    added dog endpoint

--- index.js
+++ index.js
@@ -39,6 +39,19 @@ app.get('/api/cat', (req, res) => {
   
 });
 
+app.get('/api/dog', (req, res) => {
+  const data = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  };
+  res.json(data);
+});
+
 app.get('/', (req, res) => {
   res.json({greeting: 'hello'});
 });
